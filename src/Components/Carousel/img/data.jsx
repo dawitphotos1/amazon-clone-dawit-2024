7b54534be7de1f6b89be4a8a74fcf6e7aa5750a5
Dawit <dawitphotos1@gmail.com>
2024-09-16 17:19:52 -0400

    Update

--- src/Components/Carousel/img/data.jsx
+++ src/Components/Carousel/img/data.jsx
@@ -1,34 +1,16 @@
-import img2 from "./img2.jpg";
-import img33 from "./img33.jpg";
-import img24 from "./img24.jpg";
+import img100 from "./img100.jpg";
+import img101 from "./img101.jpg";
+import img102 from "./img102.jpg";
+import img103 from "./img103.jpg";
+import img104 from "./img104.jpg";
+import img105 from "./img105.jpg";
 import img44 from "./img44.jpg";
-import img5 from "./img5.jpg";
+import img2 from "./img2.jpg";
 import img6 from "./img6.jpg";
-import img94 from "./img94.jpg";
-import img54 from "./img54.jpg";
-import img11 from "./img11.jpg";
-import img66 from "./img66.jpg";
-import img77 from "./img77.jpg";
+import img24 from "./img24.jpg";
 
-// import img7 from "./img7.jpg";
-export const img = [img94,img2, img24, img66,img5,img77, img6, img11, img33, img44, img54];
+export const img = [img100, img101, img102, img103, img104, img105, img44,img2,img6,img24]
 
-// **************************************************************
-
-
-// import img2 from "./img2.jpg";
-// import img33 from "./img33.jpg";
-// import img24 from "./img24.jpg";
-// import img44 from "./img44.jpg";
-// import img5 from "./img5.jpg";
-// import img6 from "./img6.jpg";
-// import img94 from "./img94.jpg";
-// import img54 from "./img54.jpg";
-// import img11 from "./img11.jpg";
-// import img66 from "./img66.jpg";
-// import img77 from "./img77.jpg";
-
-// const images = [
 //   img94,
 //   img2,
 //   img24,
@@ -40,22 +22,6 @@ export const img = [img94,img2, img24, img66,img5,img77, img6, img11, img33, img
 //   img33,
 //   img44,
 //   img54,
-// ];
 
-// const ImageGallery = () => {
-//   return (
-//     <div className="image-gallery">
-//       {images.map((img, index) => (
-//         <img
-//           key={index}
-//           src={img}
-//           alt={`img-${index}`}
-//           className="gallery-image"
-//         />
-//       ))}
-//     </div>
-//   );
-// };
 
-// export default ImageGallery;
-
+// **************************************************************
