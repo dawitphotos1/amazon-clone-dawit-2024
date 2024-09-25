d13ecd02618ea41d524636baca0c00e5f2aab8ed
Dawit <dawitphotos1@gmail.com>
2024-09-25 18:03:27 -0400

    update

--- src/Api/axios.jsx
+++ src/Api/axios.jsx
@@ -2,7 +2,7 @@ import axios from 'axios'
 
 
 const axiosInstance = axios.create({
-  baseURL: "http://127.0.0.1:5001/clone-dawit-2024/us-central1/api",
+  baseURL: "https://amazon-api-g3dp.onrender.com",
 });
 
 export {axiosInstance}
