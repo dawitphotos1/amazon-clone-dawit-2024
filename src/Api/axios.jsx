60a6f4a58f9119abe47fc075f5697aaa9aff8cf5
Dawit <dawitphotos1@gmail.com>
2024-09-26 11:38:05 -0400

    update

--- src/Api/axios.jsx
+++ src/Api/axios.jsx
@@ -2,6 +2,10 @@ import axios from 'axios'
 
 
 const axiosInstance = axios.create({
+  // local instance of firebase functions
+  // baseURL: "http://127.0.0.1:5001/clone-dawit-2024/us-central1/api",
+
+  // Deployed version of amazon server on render.com
   baseURL: "https://amazon-api-g3dp.onrender.com",
 });
 
